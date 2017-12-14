12b5481bb836a221ba2eafd0667f039e6d414d7b
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2017-12-14 23:53:50 +0100

    Remove non deploy files

--- docs/INSTALL.rst
+++
@@ -1,5 +0,0 @@
-test.cgwb Installation
-=================================================================================
-
-To install test.cgwb, follow this `documentation <http://plone.org/documentation/kb/installing-add-ons-quick-how-to>`_.
-
--- src/test/cgwb/Extensions/Install.py
+++
@@ -1,8 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-
-def uninstall(portal):
-    setup_tool = getToolByName(portal, 'portal_setup')
-    setup_tool.runAllImportStepsFromProfile(
-        'profile-test.cgwb:uninstall')
-    return "test.cgwb uninstalled"
